e9f2cefe41accb4e9ee6408918bce89471590e4a
Zaid EL GAOUAL <elgaoual.zaid@gmail.com>
2026-06-10 16:24:48 +0200

    Align Forge deploy command with course

--- blockchain/script/Deploy.s.sol
+++ blockchain/script/Deploy.s.sol
@@ -6,15 +6,13 @@ import {Ticket} from "../src/Ticket.sol";
 
 contract Deploy is Script {
     function run() external returns (Ticket ticket) {
-        uint256 privateKey = vm.envUint("PRIVATE_KEY");
-
         string memory name = vm.envOr("TICKET_NAME", string("Demo Ticket"));
         string memory symbol = vm.envOr("TICKET_SYMBOL", string("DTIX"));
         uint256 maxSupply = vm.envOr("MAX_SUPPLY", uint256(100));
         string memory ticketURI = vm.envOr("TICKET_URI", string("ipfs://metadata"));
         uint256 priceWei = vm.envOr("PRICE_WEI", uint256(0.01 ether));
 
-        vm.startBroadcast(privateKey);
+        vm.startBroadcast();
         ticket = new Ticket(name, symbol, maxSupply, ticketURI, priceWei);
         vm.stopBroadcast();
 
